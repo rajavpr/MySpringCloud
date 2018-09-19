03aa0ec585e7ea02abb1eea13e27e0e6a7e3cd2d
Raja <Raja@DESKTOP-KTTPSQM>
2018-09-19 10:08:03 +0530

    added load balancer logic

--- EmployeeService/src/main/java/com/raja/cloud/EmployeeService/EmployeeController.java
+++ EmployeeService/src/main/java/com/raja/cloud/EmployeeService/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.raja.cloud.EmployeeService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RefreshScope
@@ -18,10 +21,17 @@ public class EmployeeController {
 	@Value("${employee.default.role}")
 	String role;
 
+	@Autowired
+	RestTemplate restTemplate;
 	  
 	@RequestMapping("/getdefaultvalues")
 	public String getDefaultDetails() {
-		return id+" "+department+" "+role;
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("http://AddressService/address", String.class);
+		
+		System.out.println(response.getStatusCode()+" " + response.getStatusCodeValue());
+		
+		return id+" "+department+" "+role+response.getBody();
 	}
 	
 }
